ea3a9ed46ac5a2285761e4970204b4fbf5cfa337
ezequie malepalte <maleplateeze@gmail.com>
2026-09-09 18:52:46 -0300

    Agrega formulario y listado inicial de productos

--- renderer.js
+++ renderer.js
@@ -1,8 +1,47 @@
+const products = [
+  {
+    name: 'Martillo',
+    price: 8500,
+    stock: 10,
+  },
+  {
+    name: 'Destornillador',
+    price: 4200,
+    stock: 6,
+  },
+];
+
 const productsButton = document.querySelector('#products-button');
-const message = document.querySelector('#message');
+const productsList = document.querySelector('#products-list');
+const productForm = document.querySelector('#product-form');
+const productName = document.querySelector('#product-name');
+const productPrice = document.querySelector('#product-price');
+const productStock = document.querySelector('#product-stock');
 
-productsButton.addEventListener('click', () => {
-    message.textContent = 'Proximamente mostraremos los productos.';
+function showProducts() {
+  productsList.innerHTML = '';
+
+  for (const product of products) {
+    const listItem = document.createElement('li');
+
+    listItem.textContent =
+      `${product.name} - $${product.price} - Stock: ${product.stock}`;
+
+    productsList.append(listItem);
+  }
+}
+
+productsButton.addEventListener('click', showProducts);
+productForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const newProduct = {
+    name: productName.value,
+    price: Number(productPrice.value),
+    stock: Number(productStock.value),
+  };
+
+  products.push(newProduct);
+  showProducts();
+  productForm.reset();
 });
-
-
